f0d3ce486650057ee0adf906c57232b96a30cd41
shimhaeun <shy624@naver.com>
2021-05-03 18:15:47 +0900

    0503

--- app/src/main/java/org/ict/myday/ListFragment.java
+++ app/src/main/java/org/ict/myday/ListFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +60,18 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, null);
+
+        ArrayAdapter Adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,LIST_MENU);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview1);
+        listView.setAdapter(adapter);
+
+        return view;
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        //return inflater.inflate(R.layout.fragment_list, container, false);
     }
+
 }
